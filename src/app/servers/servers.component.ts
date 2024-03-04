e47aa8c6e354e519cb694a466438dfb2bd7cc380
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-03-04 17:27:34 +0530

    Point template again to an external file

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,7 +4,11 @@ import { Component } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
