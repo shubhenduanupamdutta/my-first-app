f518ec5fed0005089eb5e1b28424b7a93c4b3b5e
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-03-04 16:41:33 +0530

    Custom success alert component using inline template and style

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-success-alert',
+    template: `<h4> Your task was completed successfully! </h4>`,
+    styles: [`
+        h4 {
+            height: 50px;
+            color: green;
+            border: 1px solid green;
+            text-align: center;
+            padding-top: 10px;
+        }
+    `]
+})
+export class SuccessAlertComponent {
+
+}
